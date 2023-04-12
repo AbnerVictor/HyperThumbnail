dc83c84f9307c820f6ecf02706c5fc7eb1c9df9c
AbnerVictor <victoryyxfei@gmail.com>
2023-04-12 06:08:15 +0000

    add download of demo dataset

--- download.sh
+++ download.sh
@@ -3,6 +3,9 @@ cd datasets
 wget https://github.com/AbnerVictor/HyperThumbnail/releases/download/v0.0.1/example.zip
 unzip example.zip
 
+wget https://github.com/AbnerVictor/HyperThumbnail/releases/download/v0.0.2/demo_dataset.zip
+unzip demo_dataset.zip
+
 cd ..
 mkdir checkpoints
 cd checkpoints
